83b54c24d270ee6370ba1cb1b1fa80769a509170
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-01-19 18:01:16 +0000

    Added serialport module

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
+import * as SerialPort from "serialport";
+
 let message = "Hello World!";
 
 console.log(message);
+
+const port = new SerialPort('COM3', {
+    baudRate: 115200
+}, (err) => {
+    console.log(err.message);
+});
