24ce32c9a60d5d2ffe778ac9cd18664ccd35a91f
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-01-27 21:56:54 +0000

    Added estop and sleep commands

--- src/cli/commands.ts
+++ src/cli/commands.ts
@@ -1,4 +1,5 @@
 import { Logger, LogLevel } from "../utils/logger";
+import { timeout } from "../utils/promiseUtils";
 import { ICommandStation } from "../devices/commandStations/commandStation";
 
 type CommandFunc = (command:string[])=>Promise<boolean>;
@@ -8,6 +9,9 @@ export interface Command extends CommandFunc {
     maxArgs?: number;
     help?: () => string | string;
 }
+
+let _commandStation: ICommandStation = null;
+const _seenLocos = new Set<number>();
 
 function resolveLocoAddress(locoId: string): number {
     let address = parseInt(locoId);
@@ -39,11 +43,25 @@ export function resolveCommand(commandName: string): Command {
 }
 resolveCommand.notCommand = true;
 
-let _commandStation: ICommandStation = null;
 export function setCommandStation(commandStation: ICommandStation) {
     _commandStation = commandStation;
 }
 setCommandStation.notCommand = true;
+
+export async function estop(args: string[]) {
+    if (_seenLocos.size == 0) {
+        console.error("No locos have received commands yet.");
+        return false;
+
+    }
+    const batch = await _commandStation.beginCommandBatch();
+    for (const address of _seenLocos) {
+        batch.setLocomotiveSpeed(address, 0);
+    }
+    await batch.commit();
+    return true;
+}
+estop.help = "Emergency stop all locos which have received commands this session."
 
 export async function exit(args: string[]) {
     process.exit(0);
@@ -92,6 +110,7 @@ export async function loco_speed(args: string[]) {
         return false;
     }
 
+    _seenLocos.add(address);
     const batch = await _commandStation.beginCommandBatch();
     batch.setLocomotiveSpeed(address, speed, reverse);
     await batch.commit();
@@ -120,3 +139,17 @@ export async function loglevel(args: string[]) {
 loglevel.minArgs = 0;
 loglevel.maxArgs = 1;
 loglevel.help = "Sets the application log level.\n  Usage: loglevel [NONE|ERROR|WARNING|DISPLAY|INFO|DEBUG]";
+
+export async function sleep(args: string[]) {
+    const time = parseFloat(args[0]);
+    if (isNaN(time)) {
+        console.error(`'${time}' is not a valid sleep duration`);
+        return false;
+    }
+
+    await timeout(time);
+    return true;
+}
+sleep.minArgs = 1;
+sleep.minArgs = 1;
+sleep.help = "Pause the CLI for the specified number of seconds.\n  Usage: sleep SECONDS";
