7cdf7ee1a4b34b2d5f7cc487ba0b02c2dc3ff685
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-01-31 15:58:31 +0000

    Added connection string parser for more complex device connections

--- src/utils/parsers.spec.ts
+++ src/utils/parsers.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import "mocha";
 import "./parsers";
-import { parseCommand, splitStringEx } from "./parsers";
+import { parseCommand, splitStringEx, parseConnectionString, parseIntStrict, parseFloatStrict } from "./parsers";
 
 describe("Parsers", () => {
 
@@ -24,6 +24,28 @@ describe("Parsers", () => {
             const result = splitStringEx(null, [":"], "&", "*");
             expect(result).to.be.empty;
         })
+    })
+
+    describe("Primitive Parsers", () => {
+        describe("parseFloatStrict", () => {
+            it("should handle valid floats", () => {
+                expect(parseFloatStrict("123.45")).to.equal(123.45);
+            })
+
+            it("should reject invalid floats", () => {
+                expect(() => parseFloatStrict("foo")).to.throw("\"foo\" is not a valid float");
+            })
+        });
+
+        describe("parseIntStrict", () => {
+            it("should handle valid floats", () => {
+                expect(parseIntStrict("357")).to.equal(357);
+            })
+
+            it("should reject invalid floats", () => {
+                expect(() => parseIntStrict("bar")).to.throw("\"bar\" is not a valid integer");
+            })
+        });
     })
 
     describe("Command Parser", () => {
@@ -53,60 +75,118 @@ describe("Parsers", () => {
             expect(result).to.eql(["this is", "another test"]);
         })
 
-        it ("should parse escaped quotes", () => {
+        it("should parse escaped quotes", () => {
             const result = parseCommand("\"this is a quote: '^\"'\" another^\"quote");
             expect(result).to.eql(["this is a quote: '\"'", "another\"quote"]);
         })
 
-        it ("should parse escaped hat", () => {
+        it("should parse escaped hat", () => {
             const result = parseCommand("\"This is a hat: '^^'\" ^^");
             expect(result).to.eql(["This is a hat: '^'", "^"]);
         })
 
-        it ("should parse any escaped chat", () => {
+        it("should parse any escaped chat", () => {
             const result = parseCommand("^a^b^c^ ^d^e^f");
             expect(result).to.eql(["abc def"]);
         })
 
-        it ("should not fail if hat is last character of string", () => {
+        it("should not fail if hat is last character of string", () => {
             const result = parseCommand("a b c ^ ");
             expect(result).to.eql(["a", "b", "c", " "]);
         })
 
-        it ("should parse escaped space as last word", () => {
+        it("should parse escaped space as last word", () => {
             const result = parseCommand("a b c ^");
             expect(result).to.eql(["a", "b", "c"]);
         })
 
-        it ("should handle tripple escape characters", () => {
+        it("should handle tripple escape characters", () => {
             const result = parseCommand("^^^a ^^^  ^^^\"");
             expect(result).to.eql(["^a", "^ ", "^\""]);
         })
 
-        it ("should parse strings with multiple spaces separating words", () => {
+        it("should parse strings with multiple spaces separating words", () => {
             const result = parseCommand("   foo   bar  ");
             expect(result).to.eql(["foo", "bar"]);
         })
 
-        it ("should parse empty strings", () => {
+        it("should parse empty strings", () => {
             const result = parseCommand("");
             expect(result).to.eql([]);
         })
 
-        it ("should parse strings with only strings", () => {
+        it("should parse strings with only strings", () => {
             const result = parseCommand("    ");
             expect(result).to.eql([]);
         })
 
-        it ("should parse null strings", () => {
+        it("should parse null strings", () => {
             const result = parseCommand(null);
             expect(result).to.eql([]);
         })
 
-        it ("should trim out all white space", () => {
+        it("should trim out all white space", () => {
             const result = parseCommand("\n\r\ta\t\r\nb\n\r\tc\r\n\t");
             expect(result).to.eql(["a", "b", "c"]);
         });
+    });
+
+    describe("Connection String Parser", () => {
+        it("should correclty parse out string values", () => {
+            const config = parseConnectionString("a=foo;b=bar");
+            expect(config).to.eql({
+                a: "foo",
+                b: "bar"
+            });
+        })
+
+        it("should correclty handle single value strings", () => {
+            const config = parseConnectionString("abc=def");
+            expect(config).to.eql({
+                abc: "def"
+            });
+        })
+
+        it("should correclty handle trailing semicolons", () => {
+            const config = parseConnectionString("abc=def;");
+            expect(config).to.eql({
+                abc: "def"
+            });
+        })
+
+        it("should correcly handle custom field parsers", () => {
+            const config = parseConnectionString("path=COM3;timeout=30;threshold=0.8;foo=bar", {
+                timeout: parseIntStrict,
+                threshold: parseFloatStrict,
+                foo: () => "baz"
+            });
+            expect(config).to.eql({
+                path: "COM3",
+                timeout: 30,
+                threshold: 0.8,
+                foo: "baz"
+            });
+        })
+
+        it("should handle quotes", () => {
+            const config = parseConnectionString("\"a=b;c=d;\"=f\";g=h;\"k;foo=bar");
+            expect(config).to.eql({
+                "a=b;c=d;": "f;g=h;k",
+                foo: "bar"
+            });
+        })
+
+        it("should handle escaped characters", () => {
+            const config = parseConnectionString("a^=b=c^=d^;e^=f;bar=foo");
+            expect(config).to.eql({
+                "a=b": "c=d;e=f",
+                "bar": "foo"
+            });
+        })
+
+        it("should reject incorrectly formatted string", () => {
+            expect(() => parseConnectionString("a=b=c")).to.throw("\"a=b=c\" is not a valid key/value pair");
+        })
     });
 
 });
--- src/utils/parsers.ts
+++ src/utils/parsers.ts
@@ -1,6 +1,6 @@
 const _whitespaceChars = new Set([" ", "\t", "\r", "\n", ""]);
 
-export function splitStringEx(text: string, splitChars: Set<string> | string[], quoteChar: string, escapeChar: string): string[] {
+export function splitStringEx(text: string, splitChars: Set<string> | string[], quoteChar: string, escapeChar: string, preserveSpecials?: boolean): string[] {
     if (Array.isArray(splitChars)) splitChars = new Set(splitChars);
 
     text = text || "";
@@ -13,11 +13,13 @@ export function splitStringEx(text: string, splitChars: Set<string> | string[], 
         let c = text[i];
         if (c === quoteChar) {
             quoteMode = !quoteMode;
+            if (preserveSpecials) currentWord += c;
             continue;
         }
         if (c === escapeChar) {
             isEscaped = true;
             i++;
+            if (preserveSpecials) currentWord += c;
             c = text[i] || "";
         }
 
@@ -37,6 +39,37 @@ export function splitStringEx(text: string, splitChars: Set<string> | string[], 
     return commandArgs;
 }
 
+export function parseIntStrict(text: string): number {
+    const v = parseInt(text);
+    if (isNaN(v)) throw Error(`\"${text}\" is not a valid integer`);
+    return v;
+}
+
+export function parseFloatStrict(text: string): number {
+    const v = parseFloat(text);
+    if (isNaN(v)) throw Error(`\"${text}\" is not a valid float`);
+    return v;
+}
+
 export function parseCommand(command: string): string[] {
     return splitStringEx(command, _whitespaceChars, "\"", "^");
+}
+
+export function parseConnectionString(c: string, fieldParsers?: {[key: string]: (arg:string) => any}): {[key: string]: any} {
+    fieldParsers = fieldParsers || {};
+    const words = splitStringEx(c, [";"], "\"", "^", true);
+    const result = {};
+    for (const word of words) {
+        const kv = splitStringEx(word, ["="], "\"", "^");
+        if (kv.length != 2) throw new Error(`\"${word}\" is not a valid key/value pair`);
+        kv[0] = kv[0].trim();
+        if (kv[0] in fieldParsers) {
+            result[kv[0]] = fieldParsers[kv[0]](kv[1]);
+        }
+        else {
+            result[kv[0]] = kv[1];
+        }
+    }
+
+    return result;
 }
