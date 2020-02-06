0ca1c46a250824e5d57d4690bc16a8ad1640e2dc
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-02-06 20:50:56 +0000

    Added spawnAsync

--- src/utils/exec.ts
+++ src/utils/exec.ts
@@ -1,4 +1,5 @@
-import { exec } from "child_process";
+import { exec, spawn } from "child_process";
+import { nextTick } from "./promiseUtils";
 import { Logger } from "./logger";
 
 const log = new Logger("Exec");
@@ -29,5 +30,28 @@ export function execAsync(command: string): Promise<string> {
         catch (ex) {
             reject(ex);
         }
+    });
+}
+
+type OutputCallback = (data: string)=>void;
+export function spawnAsync(command: string, onStdOut: OutputCallback, onStdErr: OutputCallback): Promise<number> {
+    return new Promise<number>((resolve, reject) => {
+        const proc = spawn(command, {
+            shell: true,
+            windowsHide: true 
+        });
+        proc.on("exit", async (code) => {
+            await nextTick();
+            resolve(code);
+        });
+        proc.on("error", (err) => {
+            reject(err);
+        });
+        proc.stdout.on("data", (data) => {
+            onStdOut(data);
+        });
+        proc.stderr.on("data", (data) => {
+            onStdOut(data);
+        });
     });
 }
