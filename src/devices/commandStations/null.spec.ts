0379406b5eb5bb9cf263981c191a05e1f22a7d98
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-03-08 11:54:39 +0000

    Added null command station tests for CV reading/writing.

--- src/devices/commandStations/null.spec.ts
+++ src/devices/commandStations/null.spec.ts
@@ -3,6 +3,7 @@ import "mocha";
 
 import { CommandStationState } from "./commandStation"
 import { NullCommandStation, NullCommandBatch } from "./null";
+import { ensureCvNumber } from "./nmraUtils";
 
 describe("Null Command Station", () => {
     it("should open without error", async () => {
@@ -56,5 +57,61 @@ describe("Null Command Station", () => {
     it("should accept raw writes", async () => {
         const cs = await NullCommandStation.open();
         await cs.writeRaw([]);
+    })
+
+    describe("readLocoCv", () => {
+        it("should initialise default CV values", async () => {
+            const cs = await NullCommandStation.open();
+            expect(await cs.readLocoCv(1)).to.equal(3);
+            expect(await cs.readLocoCv(3)).to.equal(5);
+            expect(await cs.readLocoCv(4)).to.equal(5);
+            expect(await cs.readLocoCv(7)).to.equal(100);
+            expect(await cs.readLocoCv(8)).to.equal(255);
+            expect(await cs.readLocoCv(29)).to.equal(6);
+        })
+
+        it("should return 0 for unset CV value", async () => {
+            const cs = await NullCommandStation.open();
+            expect(await cs.readLocoCv(255)).to.equal(0);
+        })
+
+        it("should reject invalid CV number", async () => {
+            const cs = await NullCommandStation.open();
+            expect(cs.readLocoCv(0)).to.be.eventually.rejectedWith("CV 0 outside of valid range");
+        })
+    })
+
+    describe("writeLocoCv", () => {
+        it("should update existing CV value", async () => {
+            const cs = await NullCommandStation.open();
+            await cs.writeLocoCv(1, 10);
+            expect(await cs.readLocoCv(1)).to.equal(10);
+        })
+
+        it("should update unset CV value", async () => {
+            const cs = await NullCommandStation.open();
+            await cs.writeLocoCv(255, 123);
+            expect(await cs.readLocoCv(255)).to.equal(123);
+        })
+
+        it("should reject invalid CV number", async () => {
+            const cs = await NullCommandStation.open();
+            expect(cs.writeLocoCv(256, 1)).to.be.eventually.rejectedWith("CV 256 outside of valid range");
+        })
+
+        it("should reject invalid CV value", async () => {
+            const cs = await NullCommandStation.open();
+            expect(cs.writeLocoCv(1, -1)).to.be.eventually.rejectedWith("Byte(-1) outside of valid range");
+        })
+
+        it("should reject writing to CV 7", async () => {
+            const cs = await NullCommandStation.open();
+            expect(cs.writeLocoCv(7, 1)).to.be.eventually.rejectedWith("Attempted to write to readonly CV");
+        })
+
+        it("should reject writing to CV 8", async () => {
+            const cs = await NullCommandStation.open();
+            expect(cs.writeLocoCv(8, 1)).to.be.eventually.rejectedWith("Attempted to write to readonly CV");
+        })
     })
 })
