37efe53ece83bff2cabbddfc6475b78c3eefa690
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-03-08 11:27:20 +0000

    Added CV reading/writing to NullCommandStation.

--- src/devices/commandStations/null.ts
+++ src/devices/commandStations/null.ts
@@ -3,6 +3,7 @@ import { CommandStationBase, ICommandBatch, CommandStationState, ICommandStation
 import { toHumanHex } from "../../utils/hex";
 import { parseConnectionString, parseFloatStrict } from "../../utils/parsers";
 import { timeout } from "../../utils/promiseUtils";
+import { ensureCvNumber, ensureByte } from "./nmraUtils";
 
 const log = new Logger("NullCommandStation");
 
@@ -11,6 +12,14 @@ export class NullCommandStation extends CommandStationBase {
     readonly deviceId = NullCommandStation.deviceId;
     version: string = "1.0.0";
     private readonly _execTime: number;
+    private readonly _cvValues = {
+        1: 3,
+        3: 5,
+        4: 5,
+        7: 100,
+        8: 255,
+        29: 6
+    };
 
     static open(connectionString?: string): Promise<ICommandStation> {
         let cs = new NullCommandStation(connectionString);
@@ -43,6 +52,31 @@ export class NullCommandStation extends CommandStationBase {
     writeRaw(data: Buffer | number[]): Promise<void> {
         log.debug(() => `writeRaw to command station, size=${data.length} bytes=${toHumanHex(data)}`);
         return Promise.resolve();
+    }
+
+    readLocoCv(cv: number): Promise<number> {
+        try {
+            ensureCvNumber(cv);
+            return Promise.resolve(this._cvValues[cv] || 0);
+        }
+        catch (err) {
+            return Promise.reject(err);
+        }
+    }
+
+    writeLocoCv(cv: number, value: number): Promise<void> {
+        try {
+            ensureCvNumber(cv);
+            ensureByte(value);
+            if (cv === 7 || cv === 8)
+                throw new Error("Attempted to write to readonly CV");
+
+            this._cvValues[cv] = value;
+            return Promise.resolve();
+        }
+        catch(err) {
+            return Promise.reject(err);
+        }
     }
 }
 
