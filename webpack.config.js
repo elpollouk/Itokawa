29e46644c498790a9e6e55d5dafcab9657620832
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-03-13 14:28:52 +0000

    Experimenting without type checking for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,11 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        loader: 'ts-loader',
 //        exclude: /node_modules/,
+        options: {
+          transpileOnly: true
+        }
       },
       {
         test: /\.html$/i,
