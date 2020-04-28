ce74dd44b0019b60f315b0ee8170e7fcabbface2
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-04-28 15:35:02 +0100

    Bumped version number to 0.2.3. (#28)
    Added test to ensure package.json and package-lock.json are in sync.

---
+++ src/version.spec.ts
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import "mocha";
+
+let packagejson = require('../package.json');
+let packagelockjson = require('../package-lock.json');
+
+describe("Version", () => {
+    it("should match in both package.json and package-lock.json", () => {
+        // If these two values aren't kept in sync, it will break automatic updates by causing a merge conflict
+        expect(packagelockjson.version).to.equal(packagejson.version);
+    })
+})
