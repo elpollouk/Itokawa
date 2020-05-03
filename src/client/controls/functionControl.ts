e96384f81ce6377ad5ed9489e735ff5ba5655025
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-05-03 14:55:12 +0100

    Quick and dirty storing of latched status

--- src/client/controls/functionControl.ts
+++ src/client/controls/functionControl.ts
@@ -3,27 +3,52 @@ import { FunctionConfig, FunctionMode } from "../../common/api";
 
 const LATCHED_CLASS = "latchedOn";
 
+const _latchStatus = new Map<number, Map<string, boolean>>();
+
 export class FunctionControl extends ControlBase {
+    private _latchedOn: boolean;
+
     constructor(parent: HTMLElement, private readonly _locoId: number, private readonly _function: FunctionConfig) {
         super();
+        this._latchedOn = this._getLatchedStatus();
         this._init(parent);
     }
 
     protected _buildUi(): HTMLElement {
         const button = document.createElement("button");
         button.classList.add("function");
+        if (this._latchedOn) button.classList.add(LATCHED_CLASS);
         button.innerText = this._function.name;
         button.onclick = () => this._onExecute();
         return button;
     }
 
+    private _getLatchedStatus() {
+        if (this._function.mode !== FunctionMode.Latched) return false;
+        const locoStatus = _latchStatus.get(this._locoId);
+        if (!locoStatus) return false;
+        return !!locoStatus.get(this._function.exec);
+    }
+
+    private _toggleLatchedStatus() {
+        if (this._function.mode !== FunctionMode.Latched) return;
+        this._latchedOn = !this._latchedOn;
+        let locoStatus = _latchStatus.get(this._locoId);
+        if (!locoStatus) {
+            locoStatus = new Map<string, boolean>();
+            _latchStatus.set(this._locoId, locoStatus);
+        }
+        locoStatus.set(this._function.exec, this._latchedOn);
+        return this._latchedOn;
+    }
+
     private _onExecute() {
         if (this._function.mode === FunctionMode.Latched) {
-            if (this._element.classList.contains(LATCHED_CLASS)) {
-                this._element.classList.remove(LATCHED_CLASS);
+            if (this._toggleLatchedStatus()) {
+                this._element.classList.add(LATCHED_CLASS);
             }
             else {
-                this._element.classList.add(LATCHED_CLASS);
+                this._element.classList.remove(LATCHED_CLASS);
             }
         }
     }
