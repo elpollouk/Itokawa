61579bb6d3ea1419423268189e54588b0ea93e7b
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-05-10 02:17:15 +0100

    Dragging now updates the order

--- static/test/index.js
+++ static/test/index.js
@@ -3,24 +3,29 @@
 let minY;
 let maxY;
 
-let listElements = [];
+let listElement;
+let listItems = [];
 
 // Current drag state
 let startY;
+let currentTop = null;
 let draggingElement = null;
 let draggingElementTop;
 let draggingElementBottom;
 
+function getPageY(evnt) {
+    if (evnt.changedTouches) {
+        return evnt.changedTouches[0].pageY;
+    }
+    return evnt.pageY;
+}
+
 function onPointerDown(element, evnt) {
     element.classList.add("dragging");
-    if (evnt.changedTouches) {
-        startY = evnt.changedTouches[0].clientY;
-    }
-    else {
-        startY = evnt.pageY;
-    }
+    startY = getPageY(evnt);
+    currentTop = null;
 
-    for (const item of listElements) {
+    for (const item of listItems) {
         const rect = item.element.getBoundingClientRect();
         if (item.element === element) {
             draggingElementTop = rect.top;
@@ -30,11 +35,12 @@ function onPointerDown(element, evnt) {
             item.element.classList.add("shiftable");
         }
         item.top = rect.top;
-        item.bottom = rect.bottom;
+        item.mid = (rect.top + rect.bottom) / 2;
+        item.height = rect.bottom - rect.top;
     }
 
     // Calculate new list extents
-    const rect = element.parentElement.getBoundingClientRect();
+    const rect = listElement.getBoundingClientRect();
     console.log(`top=${rect.top}, bottom=${rect.bottom}`);
     minY = rect.top - 10;
     maxY = rect.bottom + 10;
@@ -50,10 +56,28 @@ function onPointerUp(element, evnt) {
 
     element.classList.remove("dragging");
     element.style.transform = "";
-    for (const item of listElements) {
-        item.element.classList.remove("shiftUp");
-        item.element.classList.remove("shiftDown");
+
+    for (const item of listItems) {
+        if (item.element === element) {
+            item.top = currentTop;
+        }
+        else if (item.element.classList.contains("shiftUp")) {
+            item.element.classList.remove("shiftUp");
+            item.top -= item.height;
+        }
+        else if (item.element.classList.contains("shiftDown")) {
+            item.element.classList.remove("shiftDown");
+            item.top += item.height;
+        }
         item.element.classList.remove("shiftable");
+    }
+
+    if (currentTop !== null) {
+        listItems.sort((a, b) => a.top - b.top);
+        listElement.innerHTML = "";
+        for (const item of listItems) {
+            listElement.appendChild(item.element);
+        }
     }
 
     draggingElement = null;
@@ -63,15 +87,7 @@ function onPointerUp(element, evnt) {
 function onPointerMove(element, evnt) {
     if (!draggingElement || draggingElement != element) return;
 
-    let pageY;
-    if (evnt.changedTouches) {
-        pageY = evnt.changedTouches[0].pageY;
-    }
-    else {
-        pageY = evnt.pageY;
-    }
-
-    let dY = pageY - startY;
+    let dY = getPageY(evnt) - startY;
 
     if ((draggingElementTop + dY) < minY) dY = minY - draggingElementTop;
     else if ((draggingElementBottom + dY) > maxY) dY = maxY - draggingElementBottom;
@@ -79,16 +95,16 @@ function onPointerMove(element, evnt) {
     element.style.transform = `translateY(${dY}px)`;
 
     let above = true;
-    let currentTop = draggingElementTop + dY;
+    currentTop = draggingElementTop + dY;
     let currentBottom = draggingElementBottom + dY;
-    for (const item of listElements) {
+    for (const item of listItems) {
         if (item.element === element) {
             above = false;
         }
-        else if (above && currentTop < item.top) {
+        else if (above && currentTop < item.mid) {
             item.element.classList.add("shiftDown");
         }
-        else if (!above && currentBottom > item.bottom) {
+        else if (!above && currentBottom > item.mid) {
             item.element.classList.add("shiftUp");
         }
         else {
@@ -115,26 +131,26 @@ function addItem(parent, text) {
 
     parent.appendChild(item);
 
-    listElements.push({
+    listItems.push({
         element: item,
-        top: -1,
-        bottom: -1
+        top: null,
+        mid: null
     });
 }
 
 function main() {
     console.log("Hello World");
 
-    const list = document.getElementById("list");
-    addItem(list, "A");
-    addItem(list, "B");
-    addItem(list, "C");
-    addItem(list, "D");
-    addItem(list, "E");
-    addItem(list, "F");
-    addItem(list, "G");
-    addItem(list, "H");
-    addItem(list, "I");
+    listElement = document.getElementById("list");
+    addItem(listElement, "A");
+    addItem(listElement, "B");
+    addItem(listElement, "C");
+    addItem(listElement, "D");
+    addItem(listElement, "E");
+    addItem(listElement, "F");
+    addItem(listElement, "G");
+    addItem(listElement, "H");
+    addItem(listElement, "I");
 
     window.onmouseup = (evnt) => onPointerUp(draggingElement, evnt);
     window.onmousemove = (evnt) => onPointerMove(draggingElement, evnt);
