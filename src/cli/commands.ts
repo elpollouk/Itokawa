c0e7b96d07b2cc9d100ae9bf70b60e2b90fc8214
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-10-16 15:18:01 +0100

    Initial refactor to move command parsing/processing into its own file

--- src/cli/commands.ts
+++ src/cli/commands.ts
@@ -1,5 +1,5 @@
 import { Logger, LogLevel } from "../utils/logger";
-import { resolveCommand, execCommand, CommandContext } from "./main"
+import { execCommand, CommandContext, error } from "./executor"
 import { timeout } from "../utils/promiseUtils";
 import * as fs from "fs";
 import { fromHex, toHumanHex } from "../utils/hex";
@@ -11,8 +11,8 @@ const _seenLocos = new Set<number>();
 
 function resolveLocoAddress(context: CommandContext, locoId: string): number {
     let address = parseInt(locoId);
-    if (isNaN(address)) context.error(`'${locoId}' is not a valid loco id`);
-    if (address < 1 || address > 9999) context.error(`'${locoId}' is not a valid loco id`)
+    if (isNaN(address)) error(`'${locoId}' is not a valid loco id`);
+    if (address < 1 || address > 9999) error(`'${locoId}' is not a valid loco id`)
 
     _seenLocos.add(address);
 
@@ -21,15 +21,15 @@ function resolveLocoAddress(context: CommandContext, locoId: string): number {
 
 function resolveSpeed(context: CommandContext, speedStr: string): number {
     let speed = parseInt(speedStr);
-    if (isNaN(speed)) context.error(`'${speedStr}' is not a valid speed value`);
-    if (speed < 0 || speed > 127) context.error(`'${speedStr}' is not a valid speed value`);
+    if (isNaN(speed)) error(`'${speedStr}' is not a valid speed value`);
+    if (speed < 0 || speed > 127) error(`'${speedStr}' is not a valid speed value`);
     return speed;
 }
 
 function resolveFunction(context: CommandContext, funcStr: string): number {
     let func = parseInt(funcStr);
-    if (isNaN(func)) context.error(`'${funcStr}' is not a valid function`);
-    if (func < 0 || func > 28) context.error(`'${funcStr}' is not a valid function`);
+    if (isNaN(func)) error(`'${funcStr}' is not a valid function`);
+    if (func < 0 || func > 28) error(`'${funcStr}' is not a valid function`);
     return func;
 }
 
@@ -61,7 +61,7 @@ echo.help = "Echo args back to the output."
 
 // Emergency stop
 export async function estop(context: CommandContext, args?: string[]) {
-    if (_seenLocos.size == 0) context.error("No locos have received commands yet.");
+    if (_seenLocos.size == 0) error("No locos have received commands yet.");
 
     const batch = await application.commandStation.beginCommandBatch();
     for (const address of _seenLocos) {
@@ -99,39 +99,15 @@ export async function exit(context: CommandContext, args?: string[]) {
 exit.maxArgs = 0;
 exit.help = "Exits this application";
 
-// Help
-export async function help(context: CommandContext, args: string[]) {
-    if (args.length == 0) {
-        context.out("Available commands:");
-        for (const commandName of Object.keys(exports)) {
-            try {
-                resolveCommand(context, commandName);
-                context.out(`  ${commandName}`);
-            }
-            catch {}
-        }
-        return;
-    }
-
-    const command = resolveCommand(context, args[0]);
-    if (!command) context.error(`Unrecognised command '${args[0]}'`);
-    if (!command.help) context.error(`${args[0]} is not helpful`);
-
-    const help = command.help instanceof Function ? command.help() : command.help;
-    context.out(help);
-}
-help.maxArgs = 1;
-help.help = "Lists available commands or retrieves help on a command\n  Usage: help [COMMAND_NAME]";
-
 function parseCvNumber(context: CommandContext, value: string) {
     const cv = parseInt(value);
-    if (isNaN(cv) || cv < 1 || cv > 255) context.error(`${value} is not a valid CV number`);
+    if (isNaN(cv) || cv < 1 || cv > 255) error(`${value} is not a valid CV number`);
     return cv;
 }
 
 function parseByte(context: CommandContext, value: string) {
     const byte = parseInt(value);
-    if (isNaN(byte) || byte < 0 || byte > 255) context.error(`${value} is not a valid byte value`);
+    if (isNaN(byte) || byte < 0 || byte > 255) error(`${value} is not a valid byte value`);
     return byte;
 }
 
@@ -196,7 +172,7 @@ export async function loglevel(context: CommandContext, args: string[]) {
     }
 
     const newLevel = args[0].toUpperCase();
-    if (!(newLevel in LogLevel)) context.error(`${newLevel} isn't a recognised log level`);
+    if (!(newLevel in LogLevel)) error(`${newLevel} isn't a recognised log level`);
 
     Logger.logLevel = LogLevel[newLevel];
 }
@@ -227,7 +203,7 @@ raw_write.help = "Write raw bytes to the command station\n  Udate: raw_write HEX
 // Sleep
 export async function sleep(context: CommandContext, args: string[]) {
     const time = parseFloat(args[0]);
-    if (isNaN(time)) context.error(`'${time}' is not a valid sleep duration`);
+    if (isNaN(time)) error(`'${time}' is not a valid sleep duration`);
 
     await timeout(time);
 }
---
+++ src/cli/executor.ts
@@ -0,0 +1,107 @@
+import { parseCommand } from "../utils/parsers";
+
+// Command function interface that specifies the available attributes
+type CommandFunc = (context: CommandContext, command:string[])=>Promise<void>;
+export interface Command extends CommandFunc {
+    notCommand?: boolean;           // Flag that the exported function is not a user command
+    minArgs?: number;               // Minimum number of args the user must supply
+    maxArgs?: number;               // Maximum number of args the user can supply
+    help?: () => string | string;   // String to display for command help
+}
+
+// Context used to allow commands to interact with their execution environment
+type Output = (message:string)=>void;
+export interface CommandContext {
+    out: Output;
+    error: Output;
+}
+
+// An exception a command can throw to display an error to the user without a call stack.
+// Should be used for user caused errors such as incorrectly specified args rather than actual failures.
+export class CommandError extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+// Utility function to raise command errors of the correct type
+export function error(message: string) {
+    throw new CommandError(message);
+}
+
+const _commands = {};
+
+// Register commands with the executor
+export function registerCommands(...commandModules: any[]) {
+    for (const module of commandModules) {
+        for (const entry in module) {
+            const command = module[entry];
+            if (!(command instanceof Function)) continue;
+            if (command.notCommand) continue;
+            _commands[entry] = command;
+        }
+    }
+}
+
+// Handler for a raw command string which captures errors.
+export async function execCommandSafe(context: CommandContext, commandString: string, suppressOk: boolean=false) {
+    try {
+        await execCommand(context, commandString, suppressOk);
+    }
+    catch (ex) {
+        if (ex instanceof CommandError) context.error(ex.message);
+        else context.error(ex);
+    }
+}
+
+// Handler for a raw command string.
+export async function execCommand(context: CommandContext, commandString: string, suppressOk: boolean=false) {
+    const commandArgs = parseCommand(commandString);
+    if (commandArgs.length == 0) return;
+
+    const commandName = commandArgs.shift();
+    const command = resolveCommand(context, commandName);
+
+    if (typeof command.minArgs !== "undefined" && commandArgs.length < command.minArgs) error(`${commandName} expects at least ${command.minArgs} args`);
+    if (typeof command.maxArgs !== "undefined" && commandArgs.length > command.maxArgs) error(`${commandName} expects at most ${command.maxArgs} args`);
+
+    await command(context, commandArgs);
+    if (!suppressOk) context.out("OK");
+}
+
+// Return the function for the specified command.
+// If the command isn't found int the Commands exports or isn't a valid command function, an exception is raised
+export function resolveCommand(context: CommandContext, commandName: string): Command {
+    commandName = commandName.toLowerCase();
+    const command = _commands[commandName] as Command;
+    if (!command) error(`Unrecognised command '${commandName}'`);
+
+    return command;
+}
+
+
+//-----------------------------------------------------------------------------------------------//
+// Built in commands
+//-----------------------------------------------------------------------------------------------//
+
+// Help
+async function help(context: CommandContext, args: string[]) {
+    if (args.length == 0) {
+        context.out("Available commands:");
+        for (const commandName of Object.keys(_commands).sort()) {
+            context.out(`  ${commandName}`);
+        }
+        return;
+    }
+
+    const command = resolveCommand(context, args[0]);
+    if (!command) error(`Unrecognised command '${args[0]}'`);
+    if (!command.help) error(`${args[0]} is not helpful`);
+
+    const help = command.help instanceof Function ? command.help() : command.help;
+    context.out(help);
+}
+help.maxArgs = 1;
+help.help = "Lists available commands or retrieves help on a command\n  Usage: help [COMMAND_NAME]";
+
+_commands["help"] = help;
