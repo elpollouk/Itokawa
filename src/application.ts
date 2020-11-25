7eb1e686fa02031fc4c95ed50abe248a1d741576
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-11-25 19:20:41 +0000

    Application now checks for restore.zip when booting.

--- src/application.ts
+++ src/application.ts
@@ -14,6 +14,7 @@ import { ServerFeatureFlags } from "./server/serverFeatureFlags";
 import { TaskManager } from "./taskmanager/taskmanager";
 import { RunInTask } from "./taskmanager/tasks/runin";
 import { registerCommandStations } from "./devices/commandStations/commandStationDirectory";
+import * as backup from "./utils/backup";
 
 const log = new Logger("Application");
 
@@ -109,6 +110,10 @@ class Application {
         applyLogLevel(args);
 
         this._dataPath = initDataDirectory(args.datadir);
+
+        // Check if there are any backup files that need restoring before starting to load data
+        await this._simpleRestoreCheck();
+
         this._configPath = this.getDataPath("config.xml");
         this._config = await loadConfig(this._configPath);
 
@@ -222,6 +227,27 @@ class Application {
 
     private _registerTasks() {
         this.taskmanager.registerTaskFactory(RunInTask);
+    }
+
+    // Checks the data directory for "restore.zip" and restores it if found
+    private async _simpleRestoreCheck() {
+        log.verbose("Checking for restore.zip...");
+        const archive = this.getDataPath("restore.zip");
+        const finalArchive = this.getDataPath(".restore.zip");
+        if (!fs.existsSync(archive)) return;
+
+        log.display(`Restoring ${archive}...`);
+        await backup.restore(archive, this._dataPath);
+
+        if (fs.existsSync(finalArchive)) {
+            log.info("Deleting old .restore.zip file...");
+            fs.unlinkSync(finalArchive);
+        }
+        
+        log.info("Renaming restore.zip to .restore.zip...");
+        fs.renameSync(archive, finalArchive);
+
+        log.info("Backup restored");
     }
 }
 
