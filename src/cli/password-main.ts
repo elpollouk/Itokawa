8193aeafb567d94fcaa4a00aef594b92793dbf67
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-11-25 01:03:07 +0000

    Password prompts and hashing

--- src/cli/password-main.ts
+++ src/cli/password-main.ts
@@ -1,38 +1,53 @@
 import * as crypto from "crypto";
+import * as read from "read";
 import { Logger, LogLevel } from "../utils/logger";
 Logger.logLevel = LogLevel.DISPLAY;
 
-async function hash(password: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-        // generate random 16 bytes long salt
-        const salt = crypto.randomBytes(16).toString("hex")
+const DEFAULT_COST = 16384;
 
-        crypto.scrypt(password, salt, 64, (err, derivedKey) => {
+async function readPassword(prompt: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        read({ prompt: prompt, silent: true }, function(err, password) {
             if (err) reject(err);
-            resolve(salt + ":" + derivedKey.toString('hex'))
+            else resolve(password);
+        })
+    });
+}
+
+function scrypt(password: string, salt: string, cost: number): Promise<string> {
+    return new Promise((resolve, reject) => {
+        crypto.scrypt(password, salt, 64, { N: cost }, (err, derivedKey) => {
+            if (err) reject(err);
+            resolve(derivedKey.toString('hex'));
         });
     })
+} 
+
+async function hash(password: string, cost: number): Promise<string> {
+    const salt = crypto.randomBytes(16).toString("hex");
+    const derivedKey = await scrypt(password, salt, cost);
+    return `scrypt$${cost}$${salt}$${derivedKey}`;
 }
 
 async function verify(password: string, hash: string): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-        const [salt, key] = hash.split(":")
-        crypto.scrypt(password, salt, 64, (err, derivedKey) => {
-            if (err) reject(err);
-            resolve(key == derivedKey.toString('hex'))
-        });
-    })
+    const [type, cost, salt, key] = hash.split("$");
+    const derivedKey = await scrypt(password, salt, parseInt(cost));
+    return key == derivedKey;
 }
 
 async function main() {
-    console.log("Hello World");
+    const password = await readPassword("New admin password: ");
+    const check = await readPassword("Repeat password: ");
+    if (password !== check) {
+        console.error("Passwords did not match!");
+        process.exit(1);
+    }
 
-    const password = "abcd1234";
-    const hashed = await hash(password);
-    console.log(`${password} -> ${hashed}`);
+    const hashed = await hash(password, DEFAULT_COST);
+    console.log(`Hash: ${hashed}`);
 
-    const verified = await verify(password, hashed);
-    console.log(`Verified = ${verified}`);
+    // This is a sanity check to make sure we can verify the password in future
+    if (!await verify(password, hashed)) throw new Error("Hash was not verified");
 }
 
 main().catch((err) => {
