561b0ad7188d24200fbf2f4a073809132d04a937
Adrian O'Grady <elpollouk@users.noreply.github.com>
2021-06-08 19:17:03 +0100

    Added sign in prompt when attempting a restricted action in the UI.

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,1 +1,7 @@
 export const COOKIE_SESSION_ID = "sessionId";
+
+export const PATH_API = "/api/v1";
+export const PATH_AUTH = "/auth";
+export const PATH_LOGOUT = "/auth/logout";
+export const PATH_MAIN = "/";
+export const PATH_WEBSOCKET = "/control/v1";
