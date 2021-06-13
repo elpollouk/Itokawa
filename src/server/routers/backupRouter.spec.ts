24520b55a7b76a3e74f0c65852be7ded99cba173
Adrian O'Grady <elpollouk@users.noreply.github.com>
2021-06-13 19:26:01 +0100

    Initial root backup page implemented

--- src/server/routers/backupRouter.spec.ts
+++ src/server/routers/backupRouter.spec.ts
@@ -7,8 +7,10 @@ import { JSDOM } from "jsdom";
 
 import * as express from "express";
 import { Express } from "express-serve-static-core";
+import * as cookieParser from "cookie-parser";
 import * as fs from "fs";
 import { application } from "../../application";
+import { Permissions } from "../sessionmanager";
 import * as backupRouter from "./backupRouter";
 
 
@@ -17,10 +19,12 @@ describe("Backup Route", () => {
     let _app: Express;
     let _backupList: string[] = null;
     let _fsExists: SinonStub = null;
+    let _smHasPermission: SinonStub = null;
 
     async function get(path: string) : Promise<void> {
         const response = await request(_app)
             .get(path)
+            .set("Cookie", ["sessionId=mock_session"])
             .expect(200);
         _dom = new JSDOM(response.text);
     }
@@ -35,11 +39,15 @@ describe("Backup Route", () => {
         _app = express();
         _app.set('view engine', 'pug');
         _app.set('views','./views');
+        _app.use(cookieParser());
         _app.use("/", await backupRouter.getRouter());
 
         stub(application, "getDataPath").returns(".test.backups/backups");
         _fsExists = stub(fs, "existsSync").withArgs(".test.backups/backups").returns(true);
         stub(fs.promises, "readdir").callsFake(() => Promise.resolve(_backupList) as any);
+        _smHasPermission = stub(application.sessionManager, "hasPermission")
+            .withArgs(Permissions.SERVER_BACKUP, "mock_session")
+            .resolves(true);
     })
 
     afterEach(() => {
@@ -56,9 +64,6 @@ describe("Backup Route", () => {
 
             const button = querySelector<HTMLAnchorElement>(".linkButton");
             expect(button.href).to.equal("createBackup");
-
-            const backupList = querySelector(".backups");
-            expect(backupList.childNodes.length).to.equal(0);
         }).slow(2000).timeout(3000)
 
         it("should return an empty list of backups if the backup directory doesn't exist", async () => {
@@ -94,6 +99,10 @@ describe("Backup Route", () => {
                 expectedName += ".zip";
                 const backupName = element.querySelector(".title").textContent;
                 expect(backupName).to.eql(expectedName);
+
+                const buttons = element.querySelectorAll<HTMLAnchorElement>(".linkButton");
+                expect(buttons[0].href).to.equal(`restore/${expectedName}`);
+                expect(buttons[1].href).to.equal(`delete/${expectedName}`);
             }
 
             const backupList = querySelector(".backups");
@@ -102,5 +111,10 @@ describe("Backup Route", () => {
             verifyBackUpEntry("backup3", backupList.children[1]);
 
         }).slow(2000).timeout(3000)
+
+        it("should reject if has no permission", async () => {
+            _smHasPermission.resolves(false);
+            await expect(get("/")).to.eventually.be.rejected;
+        })
     });
 })
--- src/server/routers/backupRouter.ts
+++ src/server/routers/backupRouter.ts
@@ -1,8 +1,11 @@
 import * as express from "express";
 import { application } from "../../application";
 import * as fs from "fs";
+import { requirePermission } from "./authRouter";
+import { Permissions } from "../sessionmanager";
 
 const _router = express.Router();
+_router.use(requirePermission(Permissions.SERVER_BACKUP))
 _router.route("/")
 .get(async (_, res) => {
     const backups: string[] = [];
