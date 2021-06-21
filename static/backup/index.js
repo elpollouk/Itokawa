fe2f07855e6ee6abbad33267ab681f3d899c01b4
Adrian O'Grady <elpollouk@users.noreply.github.com>
2021-06-21 22:12:36 +0100

    Added confirmation prompts to back up actions

--- static/backup/index.js
+++ static/backup/index.js
@@ -1,3 +1,11 @@
+function deleteBackup() {
+    return confirm("Are you sure you want to delete this back up?");
+}
+
+function restoreBackup() {
+    return confirm("Are you sure you want to restore this back up?");
+}
+
 function uploadFile(value) {
     if (value) document.getElementById("UploadForm").submit();
 }
