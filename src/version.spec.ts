04f4848b9b822994f1a5a7a53182e96f0a4dd543
Adrian O'Grady <elpollouk@gmail.com>
2021-06-26 13:58:40 +0100

    Bumped version to 0.6.0-dev

--- src/version.spec.ts
+++ src/version.spec.ts
@@ -8,5 +8,6 @@ describe("Version", () => {
     it("should match in both package.json and package-lock.json", () => {
         // If these two values aren't kept in sync, it will break automatic updates by causing a merge conflict
         expect(packagelockjson.version).to.equal(packagejson.version);
+        expect(packagelockjson.packages[""].version).to.equal(packagejson.version);
     })
 })
