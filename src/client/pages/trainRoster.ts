5127fb2f9e5490714036441d6fe255a072355bd7
Adrian O'Grady <elpollouk@users.noreply.github.com>
2023-02-04 22:54:32 +0000

    Implemented initial UI for managing trains on track

--- src/client/pages/trainRoster.ts
+++ src/client/pages/trainRoster.ts
@@ -45,7 +45,31 @@ export class TrainRosterPage extends Page {
             const addTrain = (loco: Loco) => {
                 const title = createElement(this._trains, "div", "train");
                 title.innerText = `${pad(loco.address)} - ${loco.name}`;
-                title.onclick = (ev) => nav.open(TrainEditConstructor.path, { id: loco.id });
+                title.onclick = () => {
+                    prompt.stackedPrompt(title.innerText, [
+                        { caption: "Edit", onclick: () => {
+                            nav.open(TrainEditConstructor.path, { id: loco.id })
+                        }},
+                        { caption: loco?._emphemeral?.onTrack ? "Remove from track" : "Add to track", onclick: () => {
+                            let promise: Promise<void>;
+
+                            if (loco?._emphemeral?.onTrack) {
+                                loco._emphemeral.onTrack = false;
+                                promise = this._api.removeFromTrack(loco.id);
+                            }
+                            else {
+                                loco._emphemeral = { onTrack: true }
+                                promise = this._api.addToTrack(loco.id);
+                            }
+
+                            promise.catch((err) => {
+                                console.error(err);
+                                prompt.error("Failed to update track");
+                            });
+                        }},
+                        { caption: "Cancel" }
+                    ]);
+                }
             };
 
             for (const loco of locos) 
